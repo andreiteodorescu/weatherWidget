01189242c2612b5740ee2e86d9dd53c28532a487
Andrei Teodorescu <andreiteodorescu@Andreis-MacBook-Pro.local>
2024-02-12 11:21:37 +0200

    query client provider wrapper file changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+
+const queryClient = new QueryClient();
 import WeatherWidget from './components/weatherWidget/WeatherWidget';
 
 export default function App() {
   return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <WeatherWidget location={'Cluj-Napoca,ro'} />
-  );
+    </QueryClientProvider>
+  )
 };
 
 
